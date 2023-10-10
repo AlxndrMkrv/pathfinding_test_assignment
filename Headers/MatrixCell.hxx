00f5ac234de52cde843b1a259b230b8a24f0f16d
Alexander Makarov <alexander.makarov@live.com>
2023-10-10 16:52:55 +0300

    Added comments and converter to QString

--- Headers/MatrixCell.hxx
+++ Headers/MatrixCell.hxx
@@ -5,11 +5,14 @@
 
 class MatrixCell : public Cell
 {
+    // New field for a Cell structure
     bool _invalid;
 
 public:
+    // Static method to create invalid cell
     static MatrixCell Invalid () { return MatrixCell{0, 0, true}; }
 
+    // Constructors
     MatrixCell(const uint8_t &c, const uint8_t &r) :
         Cell(c, r), _invalid(false) {}
 
@@ -18,9 +21,11 @@ public:
 
     MatrixCell(const Cell &cell) : Cell(cell), _invalid(false) {}
 
+    // Flags
     inline bool isInvalid() const { return _invalid; }
     inline bool isValid() const { return ! _invalid; }
 
+    // Equality operators
     inline bool operator==(const MatrixCell &other) const {
         return Cell::operator ==(other) && _invalid == other._invalid;
     }
@@ -29,19 +34,28 @@ public:
         return ! operator ==(other);
     }
 
+    // Converter to std::string
     operator std::string() const {
         return _invalid ? "(-, -)" : Cell::operator std::string();
     }
 
+    // Stream operator for std::ostream
     friend std::ostream& operator<<(std::ostream &os, const MatrixCell &cell) {
         os << cell._invalid ? std::string("(-, -)") : static_cast<Cell>(cell);
         return os;
     }
 
+    // Converter to QString
+    operator QString() const {
+        return _invalid ? "(-, -)"
+                        : "(" + QString::number(col) + ", " +
+                              QString::number(row) + ")";
+    }
+
+    // Stream operator for QDebug
     friend QDebug& operator<<(QDebug& out, const MatrixCell &cell)
     {
         out << QString(std::string(cell).c_str());
         return out;
     }
-
 };
