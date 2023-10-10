804702ba42d490013b7c48325063ce7397dbdcb9
Alexander Makarov <alexander.makarov@live.com>
2023-10-10 16:56:26 +0300

    Added translation to ru_RU

--- pathfinding_test_assignment_ru_RU.ts
+++ pathfinding_test_assignment_ru_RU.ts
@@ -1,3 +1,51 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ru_RU"></TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Test assignment: find path</source>
+        <translation>Тестовое задание: поиск пути</translation>
+    </message>
+    <message>
+        <source>W</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>9</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>H</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>7</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Generate</source>
+        <translation>Генерировать</translation>
+    </message>
+    <message>
+        <source>Invalid matrix size</source>
+        <translation>Некорректный размер матрицы</translation>
+    </message>
+    <message>
+        <source>Invalid width cells number</source>
+        <translation>Некорректное количество ячеек по горизонтали</translation>
+    </message>
+    <message>
+        <source>Invalid height cells number</source>
+        <translation>Некорректное количество ячеек по вертикали</translation>
+    </message>
+    <message>
+        <source>Invalid cell to add</source>
+        <translation>Невозможно добавить ячейку</translation>
+    </message>
+    <message>
+        <source>Cell %1 has no neighbours and can not be added</source>
+        <translation>Ячейка %1 не имеет соседей и не может быть добавлена</translation>
+    </message>
+</context>
+</TS>
